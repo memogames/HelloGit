30985599784538717ff4b1337e49772d0a9d6c7f
Anuchit Tum Kumprai <memo.games@gmail.com>
2016-12-17 22:20:00 +0700

    added map and setting

---
+++ HelloGit/SettingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingViewController.swift
+//  HelloGit
+//
+//  Created by Tom Anuchit on 12/17/2559 BE.
+//  Copyright © 2559 Tom Anuchit. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
